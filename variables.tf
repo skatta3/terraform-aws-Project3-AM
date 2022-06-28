bbb3b022c715cc88da9722250cc5616bb1127c8d
skatta3 <56640542+skatta3@users.noreply.github.com>
2022-06-28 20:16:03 +0530

    Terraform Modules

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-0c196d2f41c8aef79"
+}
+variable "amiID" {
+	default="ami-0ba62214afa52bec7"
+}
+variable "keyPair" {
+	default="YakrutOhioKeys"
+}
+variable "keyPair1" {
+	default="YakrutOhioKeys.pem"
+}
+
+
+
+
